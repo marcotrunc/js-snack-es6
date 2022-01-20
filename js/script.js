eb9186074ffa23aa8e411ee1b9d479c045673ab8
marcotrunc <marcotrunc@gmail.com>
2022-01-20 18:18:04 +0100

    completed ex-2

--- js/script.js
+++ js/script.js
@@ -46,3 +46,39 @@ console.log(`Il peso inferiore è: ${min}`);
 console.log(nameArray[indice]);
 const result1 = document.getElementById('ex-1');
 result1.innerText = `La bici che pesa di meno è ${nameArray[indice]}`;
+
+/*Snack2
+Creare un array di oggetti di squadre di calcio. Ogni squadra avrà diverse 
+proprietà: nome, punti fatti, falli subiti.
+Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+Generare numeri random al posto degli 0 nelle proprietà:
+Punti fatti e falli subiti.
+Infine usando la destrutturazione creiamo un nuovo array i cui elementi 
+contengono solo nomi e falli subiti e stampiamo tutto in console.*/
+// ** Funzione che mi genera dei numeri casuale
+const generateRandom = () => {
+    const random = Math.floor(Math.random() * (100 - 50 + 1)) + 50;
+    return random
+}
+
+const teams = [
+    { nameTeam: 'inter' },
+    { nameTeam: 'juventus' },
+    { nameTeam: 'milan' },
+    { nameTeam: 'atalanta' },
+];
+
+const nameTeamArray = [];
+// Aggiungo nuovi elementi all'array
+for (let k = 0; k < teams.length; k++) {
+    teams[k]['point'] = generateRandom();
+    teams[k]['foluF'] = generateRandom();
+    teams[k]['foluS'] = generateRandom();
+
+    const team = teams[k];
+    const { nameTeam } = team;
+    nameTeamArray.push(nameTeam);
+
+}
+console.log(teams);
+console.log(nameTeamArray);
