20f4a38c2681422c09286b54f5c9165b71f98755
marcotrunc <marcotrunc@gmail.com>
2022-01-20 16:11:00 +0100

    completed the first ex

--- js/script.js
+++ js/script.js
@@ -6,4 +6,33 @@ da corsa con le seguenti proprietà: nome e peso.
 Stampare in console la bici con peso minore utilizzando
 destructuring e template literal */
 
-// ** Creo Un array
+// ** Creo Un array di bici da corsa
+
+const bicycles = [
+    { nameBrand: 'canyon', weight: 6.7 },
+    { nameBrand: 'rose', weight: 6.9 },
+    { nameBrand: 'colnago', weight: 5.2 },
+    { nameBrand: 'sarto', weight: 5.7 },
+]
+
+console.log(bicycles);
+
+
+const weightArray = [];
+for (let i = 0; i < bicycles.length; i++) {
+    const bicycle = bicycles[i];
+    const { weight } = bicycle;
+    weightArray.push(weight)
+}
+console.log(weightArray);
+
+
+let result = 0;
+for (let j = 1; j < weightArray.length; j++) {
+    if (weightArray[j - 1] < weightArray[j]) {
+        result = weightArray[j - 1];
+    } else {
+        result = weightArray[j];
+    }
+}
+console.log(`Il peso inferiore è:${result}`);
