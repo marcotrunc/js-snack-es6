8b453dfbfb354a6eb26ec73a5305a1129cf58bc2
marcotrunc <marcotrunc@gmail.com>
2022-01-20 15:29:26 +0100

    crated js-snack-es6 repo

---
+++ js/script.js
@@ -0,0 +1,9 @@
+console.log('JS OK');
+/*Snack 1
+Creare un array di oggetti:
+Ogni oggetto descriverà una bici
+da corsa con le seguenti proprietà: nome e peso.
+Stampare in console la bici con peso minore utilizzando
+destructuring e template literal */
+
+// ** Creo Un array
